ba01c0fd0c13ae89baecebfb8f20e8c2b9455c74
Alex-Gorodov <a.gorodov.22@gmail.com>
2026-04-15 05:12:47 +0300

    page 404

--- src/pages/Employees/EmployeesPage.tsx
+++ src/pages/Employees/EmployeesPage.tsx
@@ -1,21 +1,47 @@
 import { useSelector } from "react-redux";
-import EmployeeItem from "../../components/EmployeeItem/EmployeeItem";
 import Layout from "../../components/Layout/Layout";
+import RoleLabel from "../../components/ui/RoleLabel";
+import { Roles } from "../../const";
 
 export default function EmployeesPage() {
   const users = useSelector((state: any) => state.data.users);
+
+  const getRoleObject = (roleValue: string) => {
+    return Roles.find(r => r.value === roleValue);
+  };
 
   return (
     <Layout>
       <div className="page page--employees">
         <h1 className="page__title">רשימת עובדים</h1>
-        <div className="employees-list">
-          {
-            users?.map((user: any) => (
-              <EmployeeItem key={user.id} user={user} />
-            ))
-          }
-        </div>
+
+        <table className="employees-table">
+          <thead>
+            <tr>
+              <th>שם</th>
+              <th>תפקידים</th>
+              <th>הדרכות</th>
+              <th>טלפון</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users?.map((user: any) => (
+                <tr key={user.id}>
+                  <td>{user.firstName} {user.secondName}</td>
+                  <td className="employee__roles">
+                    {user.roles?.map((roleValue: any) => {
+                      const roleObj = getRoleObject(roleValue);
+                      return roleObj ? <RoleLabel key={roleValue} role={roleObj} /> : null;
+                    })}
+                  </td>
+                  <td>הדרכות</td>
+                  <td>{user.phoneNumber || '-'}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     </Layout>
   );
