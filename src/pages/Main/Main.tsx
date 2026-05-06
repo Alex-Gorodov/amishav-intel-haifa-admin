f6790cd998883c2d99ef926e8873dfd267150985
Alex-Gorodov <a.gorodov.22@gmail.com>
2026-05-06 20:04:30 +0300

    move main from components to pages

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Layout from '../Layout/Layout';
+import Layout from '../../components/Layout/Layout';
 
 export default function App() {
   const [date, setDate] = useState(new Date());
